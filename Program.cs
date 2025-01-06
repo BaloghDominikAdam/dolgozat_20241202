e6e2d5df844d92e479595f4ab786135c5d83e845
balogh.dominik <balogh.dominik@28-DIAK8>
2025-01-06 14:00:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dolgozat_20241202
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
